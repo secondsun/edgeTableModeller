ae8669af5137a887d785d9720e25022a784793da
Hoyt Summers Pittman <secondsun@gmail.com>
2022-04-19 10:29:33 -0400

    updates

--- src/main/java/dev/secondsun/edgemodeller/vo/Project.java
+++ src/main/java/dev/secondsun/edgemodeller/vo/Project.java
@@ -18,14 +18,14 @@ import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
+
 /**
  * Projects are the root element of a scene we are modelling. They contain the references to the textures as well as the BSP tree we are going to display.
  */
-public class Project implements Externalizable {
+public final class Project implements Externalizable {
 
-    public Project() {}
-    private final Resources resources = new Resources();
+    public Project() { /*  Enforce Externalizable Constructor Contract */ }
+    private Resources resources = new Resources();
     public record Resource(BufferedImage texture, Integer id){};
 
     private BSPTree tree = new BSPTree();
@@ -58,16 +58,56 @@ public class Project implements Externalizable {
     }
     @Override
     public void readExternal(ObjectInput arg0) throws IOException, ClassNotFoundException {
-        
+        Gson gson = new Gson();
+        tree = gson.fromJson(arg0.readObject().toString(), BSPTree.class);
+        resources = (Resources) arg0.readObject();
     }
 
     @Override
     public void writeExternal(ObjectOutput arg0) throws IOException {
         Gson gson = new Gson();
-        var json = gson.toJson(tree);
-        arg0.writeObject(json);
-        
+        var modelJson = gson.toJson(tree);
+        arg0.writeObject(modelJson);
+        //resources implemented externalizable
+        arg0.writeObject(resources);
         
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((resources == null) ? 0 : resources.hashCode());
+        result = prime * result + ((tree == null) ? 0 : tree.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Project other = (Project) obj;
+        if (resources == null) {
+            if (other.resources != null)
+                return false;
+        } else if (!resources.equals(other.resources))
+            return false;
+        if (tree == null) {
+            if (other.tree != null)
+                return false;
+        } else if (!tree.equals(other.tree))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Project [resources=" + resources + ", tree=" + tree + "]";
+    }
+
 
 }
